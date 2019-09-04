d29261e61f5128bec5ea9978ce75aed3de4b41ac
Arthur <arthur.richards@bristol.ac.uk>
2019-09-04 09:25:54 +0100

    Spawning utility and vector of agents working.

--- signworld.cpp
+++ signworld.cpp
@@ -4,22 +4,34 @@
 
 World w(30,50);
 
-//std::vector<Agent> agents;
+std::vector<Agent> agents;
+
+bool spawn_agent(ord x, ord y, ord gx, ord gy) {
+  if (w.get_occ(x,y)) {
+    return(false);
+  } else {
+    agents.push_back(Agent(&w,Location(x,y)));
+    agents.back().set_goal(gx,gy);
+    return(true);
+  }
+}
 
 // MAIN ++++++++++++++++++++++++++++++++++++
 
 int main() {
+  unsigned long int ii;
+
+  for (ii=0;ii<4;ii++) {
+    spawn_agent(15+ii,43,2,3*ii);
+  }
+  for (ii=0;ii<4;ii++) {
+    spawn_agent(17+ii,43,1,3*ii);
+  }
   w.print();
 
-  Agent b(&w,Location(28,48));
-
-  Agent a(&w,15,44);
-  a.print();
-  w.print();
-
-  a.move(5,44);
-  a.print();
-  w.print();
+  for (ii=0;ii<agents.size();ii++) {
+    agents.at(ii).print();
+  }
 
   return(0);
 };
