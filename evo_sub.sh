b7908361d7da495689ea15c52457f19939ae9080
Arthur Richards <aeagr@newblue4.cm.cluster>
2019-10-03 18:25:53 +0100

    New sub files for the refreshment option

--- evo_sub.sh
+++ evo_sub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=12:00:00
-#PBS -t 1-10
+#PBS -t 1-14
 cd $PBS_O_WORKDIR
 echo "Array ID=${PBS_ARRAYID}"
 ARG_STR=$(head -n ${PBS_ARRAYID} params.txt | tail -1)
