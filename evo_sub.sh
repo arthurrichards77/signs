91b789944e545b9aca206d5cc826bb8b63ed1386
Arthur Richards <aeagr@newblue4.cm.cluster>
2019-11-09 22:19:11 +0000

    Modified sub script to suit extra weight command line options.

--- evo_sub.sh
+++ evo_sub.sh
@@ -6,5 +6,5 @@ cd $PBS_O_WORKDIR
 echo "Array ID=${PBS_ARRAYID}"
 ARG_STR=$(head -n ${PBS_ARRAYID} params.txt | tail -1)
 echo "Arguments are ${ARG_STR}"
-# evo pop_size prob_mut prob_sel
-./evo ${ARG_STR}
+# evo pop_size prob_mut prob_sel prob_rep w_signs w_bits
+./evo ${ARG_STR} 0.001 0.001
