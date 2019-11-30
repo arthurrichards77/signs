8207564c164e15f396e5be18dfe123b8b350b535
Arthur Richards <aeagr@newblue2.cm.cluster>
2019-11-30 14:15:31 +0000

    Sorted out HPC arrangement in new folder structure.

--- hpc_exmpl/evo_sub.sh
+++ hpc_exmpl/evo_sub.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=24:00:00
-#PBS -t 1-14
 cd $PBS_O_WORKDIR
 echo "Array ID=${PBS_ARRAYID}"
 ARG_STR=$(head -n ${PBS_ARRAYID} params.txt | tail -1)
 echo "Arguments are ${ARG_STR}"
 # evo pop_size prob_mut prob_sel prob_rep w_signs w_bits
-./evo ${ARG_STR} 0.01 0.01
+../evo ${ARG_STR}
---
+++ hpc_exmpl/make_setup.py
@@ -0,0 +1,19 @@
+from sim_builder import SimBuilder
+
+s = SimBuilder(128,128)
+
+s.make_agent((10,10),[(20,10),(10,10)])
+
+s.spread_agents_on_line((90,90),(90,60),[(90,60),(10,60),(110,70)],12)
+
+s.two_way_trunk((50,5),(50,120),10)
+
+s.two_way_trunk((100,100),(30,20),10)
+
+s.two_way_trunk((100,100),(90,20),10)
+
+s.two_way_trunk((100,100),(10,90),10)
+
+s.two_way_trunk((100,100),(120,120),10)
+
+s.spread_agents_on_circuit([(15,88),(67,91),(120,119),(104,4),(73,35)],10)
