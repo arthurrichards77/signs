172bb70815f8b520d1a159305028310f6d6fe710
Arthur Richards <aeagr@newblue2.cm.cluster>
2021-07-14 14:17:55 +0100

    Second HPC example with different parameters running in isolation.

---
+++ examples/hpc_no_travel/launchjob.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python make_setup.py > setup.txt
+array_size=$(cat params.txt | wc -l)
+echo "Submitting array of $array_size jobs"
+qsub evo_sub.sh -t 1-$array_size
+qstat -t -u $LOGNAME
