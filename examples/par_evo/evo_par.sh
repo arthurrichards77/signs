f3d3f9b5310e58f8bc3f0037b0e99019651f8d27
Arthur <arthur.richards@bristol.ac.uk>
2021-07-14 12:47:48 +0100

    Updating parallel evo example for better logging and extra option.

--- examples/par_evo/evo_par.sh
+++ examples/par_evo/evo_par.sh
@@ -1,13 +1,13 @@
 # build the scenario
 python make_setup.py > setup.txt
 # command for one run
-EVO_RUN='../../bin/evo 10 0.2 0.3 0.1 0.001 0.000001'
+EVO_RUN='../../bin/evo 10 0.2 0.3 0.1 0.001 0.000001 0.01'
 # run four evolutions in parallel
 # they will communicate via 'traveller.csv'
-${EVO_RUN} | grep "Saving new" &
+${EVO_RUN} | tee log1.txt | grep "Saving new" &
 sleep 2 # need a sleep between each one so they randomize differently
-${EVO_RUN} | grep "Saving new" &
+${EVO_RUN} | tee log2.txt | grep "Saving new" &
 sleep 2
-${EVO_RUN} | grep "Saving new" &
+${EVO_RUN} | tee log3.txt | grep "Saving new" &
 sleep 2
-${EVO_RUN} | grep "Saving new"
+${EVO_RUN} | tee log4.txt | grep "Saving new"
