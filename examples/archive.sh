19f0b65081b3dd0c12515841ab8d592dae3c5bd8
Arthur Richards <aeagr@newblue2.cm.cluster>
2021-07-18 14:37:54 +0100

    Minor fix to archive script in case of trailing slash.

--- examples/archive.sh
+++ examples/archive.sh
@@ -4,6 +4,6 @@ then
   echo "$0 DIRNAME"
   exit
 fi
-filename=$1-$(date +%Y-%m-%d-%H-%M).tar.gz
+filename=${1%/}-$(date +%Y-%m-%d-%H-%M).tar.gz
 echo "Archiving $1 to $filename"
 tar -czvf $filename $1
