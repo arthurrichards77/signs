c11b8441097d14537cbf542214b9014105a70584
Arthur <arthur.richards@bristol.ac.uk>
2021-07-25 10:01:21 +0100

    tried fixing lanes but no simple answer.

--- examples/sim_only/lane_signs.py
+++ examples/sim_only/lane_signs.py
@@ -2,62 +2,84 @@ import sys, os
 sys.path.append(os.getcwd()+'/../../lib')
 from sign_builder import make_mask, make_sign, save_signs, gray
 
+even_mask = (1,1)
+odd_mask = (1,0)
+
+# can't go left in odd Y lanes
 s1 = make_sign((0,0), # applies to everyone
                (0,0),
-               (1,0),
+               odd_mask,
                (0,0),
                (0,0),
                (3,0)) 
 
 s2 = make_sign((0,0),
                (0,0),
-               (1,0),
+               odd_mask,
                (0,0),
                (0,0),
                (6,4)) 
 
+# can't go right in even Y lanes
 s3 = make_sign((0,0),
                (0,0),
-               (1,1),
+               even_mask,
                (0,0),
                (0,0),
                (3,2)) 
 
 s4 = make_sign((0,0),
                (0,0),
-               (1,1),
+               even_mask,
                (0,0),
                (0,0),
                (6,2)) 
 
+# can't go UP in odd X lanes
 s5 = make_sign((0,0),
-               (1,0),
+               odd_mask,
                (0,0),
                (0,0),
                (0,0),
                (6,0)) 
 
 s6 = make_sign((0,0),
-               (1,0),
+               odd_mask,
                (0,0),
                (0,0),
                (0,0),
                (5,1)) 
 
+# can't go DOWN in even X lanes
 s7 = make_sign((0,0),
-               (1,1),
+               even_mask,
                (0,0),
                (0,0),
                (0,0),
                (6,6)) 
 
 s8 = make_sign((0,0),
-               (1,1),
+               even_mask,
                (0,0),
                (0,0),
                (0,0),
                (5,5)) 
 
-signs = [s1,s2,s3,s4,s5,s6,s7,s8]
+# if you get to the top, dumped to the corner
+s9 = make_sign((0,0),
+               (64,0), # 1XXX 
+               (255,gray(127)), # at the very top
+               (0,0),
+               (0,0),
+               (6,6)) 
+
+s10 = make_sign((0,0),
+               (64,0), # 1XXX 
+               (255,gray(127)), # at the very top
+               (0,0),
+               (0,0),
+               (5,5)) 
+
+signs = [s1,s2,s3,s4,s5,s6,s7,s8,s9,s10]
     
 save_signs(signs,'lane_signs.csv')
