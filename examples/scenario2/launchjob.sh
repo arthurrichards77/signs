ad32c246fbaf6eb2c9022aecfafe7294c1c6a05d
Arthur <arthur.richards@bristol.ac.uk>
2021-08-03 16:41:59 +0100

    Made the setup.txt scenario descriptions tracked so they're consistent on different platforms, especially different python versions.

--- examples/scenario2/launchjob.sh
+++ examples/scenario2/launchjob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm *.sh.*
 rm *.csv
-python make_setup.py > setup.txt
+# remove as inconsistent random number generation across versions
+# python make_setup.py > setup.txt
 array_size=$(cat params.txt | wc -l)
 echo "Submitting array of $array_size jobs"
 qsub evo_sub.sh -t 1-$array_size
--- examples/scenario2a/launchjob.sh
+++ examples/scenario2a/launchjob.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 rm *.sh.*
 rm *.csv
-python make_setup.py > setup.txt
+#get setup.txt from source as generation is inconsitent across python versions
+#python make_setup.py > setup.txt
 array_size=$(cat params.txt | wc -l)
 echo "Submitting array of $array_size jobs"
 qsub evo_sub.sh -t 1-$array_size
