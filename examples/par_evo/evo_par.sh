82d4ab9907464a0b236778fa627625705b1c395f
Arthur Richards <aeagr@newblue4.cm.cluster>
2021-08-04 11:18:54 +0100

    Update to simple parallel evolution, which could be a future route to a better HPC implementation.

--- examples/par_evo/evo_par.sh
+++ examples/par_evo/evo_par.sh
@@ -5,13 +5,11 @@ rm traveller.csv
 # build the scenario
 python make_setup.py > setup.txt
 # command for one run
-EVO_RUN='../../bin/evo 10 0.2 0.3 0.1 0.001 0.000001 0.01'
+EVO_RUN='../../bin/evo 10 0.2 0.3 0.1 0.001 0.000001 0.01 0'
 # run four evolutions in parallel
 # they will communicate via 'traveller.csv'
 ${EVO_RUN} | tee log1.txt | grep "Saving new" &
-sleep 2 # need a sleep between each one so they randomize differently
 ${EVO_RUN} | tee log2.txt | grep "Saving new" &
-sleep 2
 ${EVO_RUN} | tee log3.txt | grep "Saving new" &
-sleep 2
-${EVO_RUN} | tee log4.txt | grep "Saving new"
+${EVO_RUN} | tee log4.txt | grep "Saving new" &
+wait
