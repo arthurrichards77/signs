085c4711d8b08d96bda728eabc44832e0227a106
Arthur Richards <aeagr@newblue2.cm.cluster>
2021-08-05 11:13:24 +0100

    Extra scenario and tweaks to job scripts

---
+++ examples/scenario4/showme.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#python make_setup.py > setup.txt
+../../bin/simtest result.csv $1
+if [ -z $1 ]
+then
+  filestub='nosigns'
+else
+  filestub="$1"
+  python ../../lib/plot_signs_depots.py $1
+fi
+python ../../lib/visualize_depots.py result.csv $filestub.0.png 0
+python ../../lib/visualize_depots.py result.csv $filestub.99.png 99
+python ../../lib/visualize_depots.py result.csv $filestub.199.png 199
+python ../../lib/visualize_depots.py result.csv $filestub.299.png 299
+python ../../lib/visualize_depots.py result.csv $filestub.399.png 399
+python ../../lib/visualize_depots.py result.csv $filestub.499.png 499
+python ../../lib/visualize_depots.py result.csv $filestub.mp4
